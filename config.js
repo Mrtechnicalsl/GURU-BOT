37fc5a74cd9d197ddc1c1009dc2cccf504bc6936
Mrtechnicalsl <152056532+Mrtechnicalsl@users.noreply.github.com>
2023-11-26 15:43:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94789005218', 'Mr technical', true],
+  ['94768026921', 'Mr technical', true], 
   [''] 
 ] //Number of owners
 
